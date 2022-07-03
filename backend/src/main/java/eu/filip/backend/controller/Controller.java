0f035739ae3565c6a25edd915f482b9d63c24ee5
Filip <szymanski.fp@gmail.com>
2022-07-03 15:06:18 +0200

    implemented jwt generation on correct credentials

---
+++ backend/src/main/java/eu/filip/backend/controller/Controller.java
@@ -0,0 +1,54 @@
+package eu.filip.backend.controller;
+
+import eu.filip.backend.model.AuthenticationResponse;
+import eu.filip.backend.model.LoginCredentials;
+import eu.filip.backend.service.UserDetailsServiceImpl;
+import eu.filip.backend.util.JwtUtil;
+import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody LoginCredentials loginCredentials) throws Exception{
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginCredentials.getUsername(),
+                            loginCredentials.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e){
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginCredentials.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ backend/src/main/java/eu/filip/backend/model/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package eu.filip.backend.model;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
