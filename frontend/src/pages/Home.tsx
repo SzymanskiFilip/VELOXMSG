95ee482c8245462997330ca781cdf41a3fbc6687
Filip <szymanski.fp@gmail.com>
2022-07-04 11:55:39 +0200

    added slide animation to sidebar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import {useState} from "react";
+
 function Home(): JSX.Element{
     //TODO: on the bottom a random chat button
+
+    const [sidebar, setSidebar] = useState<boolean>(false);
+    function sidebarHandler(){
+        setSidebar(!sidebar);
+        console.log(sidebar);
+    }
+
     return(
         <div>
             <nav className="bg-black text-white text-center font-bold flex flex-row items-center justify-between pt-4 pb-4">
-                <div className="w-6 ml-4">
+                <div className="w-6 ml-4 hover:cursor-pointer" onClick={sidebarHandler}>
                     <div className="bg-white w-6 mb-1 height"></div>
                     <div className="bg-white w-6 mb-1 height"></div>
                     <div className="bg-white w-6 mb-1 height"></div>
@@ -12,6 +21,11 @@ function Home(): JSX.Element{
                 <h1 className="absolute left-1/2 transform -translate-x-1/2">VELOXMSG</h1>
             </nav>
 
+            <div className={sidebar ? "slide slide-pos" : "slide"}>
+                <h1>Contacts</h1>
+                <h1>Active Users</h1>
+                <h1>Random Chat</h1>
+            </div>
         </div>
     );
 }
