878095e682ea7cdc73b896c14f0c73cb2208244f
Filip <szymanski.fp@gmail.com>
2022-07-14 23:47:40 +0200

    added messages fetching endpoint

--- backend/src/main/java/eu/filip/backend/controller/ChatController.java
+++ backend/src/main/java/eu/filip/backend/controller/ChatController.java
@@ -5,6 +5,7 @@ import eu.filip.backend.entity.Message;
 import eu.filip.backend.entity.Room;
 import eu.filip.backend.service.ChatDataService;
 import org.hibernate.boot.archive.scan.spi.PackageInfoArchiveEntryHandler;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,10 @@ public class ChatController {
     }
 
     @GetMapping("/chat/{id}")
-    public List<MessageDto> getMessages(@PathVariable Long id, Principal principal){
-        return chatDataService.getMessagesForUserInRoom(principal, id);
+    public ResponseEntity<List<MessageDto>> getMessages(@PathVariable Long id, Principal principal){
+        return ResponseEntity.ok(chatDataService.getMessagesForUserInRoom(principal, id));
     }
+
+
 
 }
