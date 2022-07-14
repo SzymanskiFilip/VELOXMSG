5f604894c842b71d9b38c02728d3183fddcb940d
Filip <szymanski.fp@gmail.com>
2022-07-14 23:48:03 +0200

    added message fetching

--- backend/src/main/java/eu/filip/backend/repository/MessageReposiroty.java
+++ backend/src/main/java/eu/filip/backend/repository/MessageReposiroty.java
@@ -1,9 +1,16 @@
 package eu.filip.backend.repository;
 
 import eu.filip.backend.entity.Message;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MessageReposiroty extends CrudRepository<Message, Long> {
+
+    @Query(nativeQuery = true, value = "select * from messages where room_id = ?1")
+    Optional<List<Message>> findMessagesFromChatId(Long chatId);
 }
