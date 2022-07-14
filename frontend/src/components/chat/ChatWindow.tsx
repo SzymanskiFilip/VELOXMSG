500359e629fd6806772a46671d1a601a69ba1dae
Filip <szymanski.fp@gmail.com>
2022-07-14 15:01:12 +0200

    scrolling do bottom as default

--- frontend/src/components/chat/ChatWindow.tsx
+++ frontend/src/components/chat/ChatWindow.tsx
@@ -1,12 +1,35 @@
 import ChatRow from "./ChatRow";
+import {useEffect, useRef} from "react";
 
 function ChatWindow(): JSX.Element {
+
+    const bottomRef = useRef<null | HTMLDivElement>(null);
+
+    const messages = [
+        {sender: "Dave", body: "hello", me: false},
+        {sender: "Filip", body: "Hi", me: true},
+        {sender: "Dave", body: "hello", me: false},
+        {sender: "Filip", body: "what's up?", me: true},
+        {sender: "Dave", body: "hello", me: false},
+        {sender: "Filip", body: "Hi", me: true},
+        {sender: "Dave", body: "hello", me: false},
+        {sender: "Filip", body: "what's up?", me: true}
+    ];
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({behavior: "smooth"});
+    }, []);
+
     return (
         <div className="w-screen sm:w-2/3 chat-window">
             <h1 className="text-center">First Chat Room</h1>
-            <div className="bg-green-200 rounded border-2 border-black h-4/5">
-                <ChatRow data={{sender: "Dave", body: "Hello!", me: false}}/>
-                <ChatRow data={{sender: "Filip", body: "Hi!", me: true}}/>
+            <div className="bg-green-200 rounded border-2 border-black h-4/5 overflow-y-scroll">
+                {
+                    messages.map(m => {
+                        return <ChatRow data={m}/>
+                    })
+                }
+                <div ref={bottomRef}></div>
             </div>
             <div className="flex flex-row items-center justify-between mr-2 ml-2">
                 <input type="text" className="outline-none border-2 w-full font-light"/>
