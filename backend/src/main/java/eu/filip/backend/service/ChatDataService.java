d6fa4fafab65c635cb98734fa8d200212b67b767
Filip <szymanski.fp@gmail.com>
2022-07-14 23:48:25 +0200

    added business logic of fetching messages

--- backend/src/main/java/eu/filip/backend/service/ChatDataService.java
+++ backend/src/main/java/eu/filip/backend/service/ChatDataService.java
@@ -1,6 +1,7 @@
 package eu.filip.backend.service;
 
 import eu.filip.backend.dto.MessageDto;
+import eu.filip.backend.entity.Message;
 import eu.filip.backend.entity.Room;
 import eu.filip.backend.entity.User;
 import eu.filip.backend.repository.RoomRepository;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,12 +20,14 @@ public class ChatDataService {
     private UserService userService;
     private JwtUtil jwtUtil;
     private ParticipantService participantService;
+    private MessageService messageService;
 
-    public ChatDataService(RoomRepository roomRepository, UserService userService, JwtUtil jwtUtil, ParticipantService participantService) {
+    public ChatDataService(RoomRepository roomRepository, UserService userService, JwtUtil jwtUtil, ParticipantService participantService, MessageService messageService) {
         this.roomRepository = roomRepository;
         this.userService = userService;
         this.jwtUtil = jwtUtil;
         this.participantService = participantService;
+        this.messageService = messageService;
     }
 
     public List<Room> getRoomsForUser(Principal principal){
@@ -38,9 +42,27 @@ public class ChatDataService {
         User user = userService.findUserByUsername(name);
 
         boolean isUserInChat = participantService.isUserInRoom(user.getId(), roomId);
-        System.out.println(isUserInChat);
 
-        return Arrays.asList();
+        if(isUserInChat){
+            List<Message> messages = messageService.getMessages(roomId);
+            List<MessageDto> messageDtos = new ArrayList<>();
+            messages.forEach((message) -> {
+                boolean me = false;
+                if(user.getId() == message.getUser_id()){
+                    me = true;
+                }
+                messageDtos.add(new MessageDto(
+                        message.getId(),
+                        message.getRoom_id(),
+                        message.getUser_id(),
+                        message.getMessage(),
+                        me
+                ));
+            });
+
+            return messageDtos;
+        }
+        return null;
     }
 
 }
--- backend/src/main/java/eu/filip/backend/service/MessageService.java
+++ backend/src/main/java/eu/filip/backend/service/MessageService.java
@@ -1,4 +1,25 @@
 package eu.filip.backend.service;
 
+import eu.filip.backend.dto.MessageDto;
+import eu.filip.backend.entity.Message;
+import eu.filip.backend.repository.MessageReposiroty;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class MessageService {
+    private MessageReposiroty messageReposiroty;
+
+    public MessageService(MessageReposiroty messageReposiroty) {
+        this.messageReposiroty = messageReposiroty;
+    }
+
+    public List<Message> getMessages(Long chatId){
+        return messageReposiroty.findMessagesFromChatId(chatId)
+                .orElseThrow(() -> new ResourceAccessException("Cant find messages"));
+    }
 }
